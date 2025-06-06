923e0a01b472c86955046a36208cc2d3133b8487
Abhijith-Reddy-ch <abhijithreddychalamalla@gmail.com>
2025-06-06 16:59:36 +0000

    Code challenge submission for question: 389 Find the Difference

---
+++ 389 Find the Difference/389find-the-difference.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char ans;
+        int len = s.length();
+
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+  
+    for(int i=0;i<len;i++){
+        if(s[i]!=t[i]){ 
+            ans = t[i];
+            break;
+            }
+    }
+    return ans;
+    }
+};
