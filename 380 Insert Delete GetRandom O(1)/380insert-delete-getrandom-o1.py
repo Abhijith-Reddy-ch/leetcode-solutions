f4f21057e9a58e81f88fc6c11bd803f460847ccc
Abhijith-Reddy-ch <abhijithreddychalamalla@gmail.com>
2025-12-21 06:03:59 +0000

    Code challenge submission for question: 380 Insert Delete GetRandom O(1)

---
+++ 380 Insert Delete GetRandom O(1)/380insert-delete-getrandom-o1.py
@@ -0,0 +1,21 @@
+import random
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.set = []
+
+    def insert(self, val: int) -> bool:
+        if val not in self.set:
+            self.set.append(val)
+            return True
+        return False
+
+    def remove(self, val: int) -> bool:
+        if val in self.set:
+            self.set.remove(val)
+            return True
+        return False
+
+    def getRandom(self) -> int:
+        return random.choice(self.set)
