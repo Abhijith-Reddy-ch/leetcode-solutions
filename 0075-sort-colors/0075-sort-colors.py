b7c37258ee238d711d99a21b7f13ac2307800b80
Abhijith Reddy Chalamalla <167357546+Abhijith-Reddy-ch@users.noreply.github.com>
2026-06-04 22:35:02 +0530

    Time: 0 ms (100%), Space: 19.3 MB (22.34%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,7 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        '''c0 =c1 =c2 =0
+        c0 =c1 =c2 =0
         for i in range (len(nums)):
             if nums[i] == 0:
                 c0 += 1
@@ -16,6 +16,4 @@ class Solution:
         for k in range (c0,c0+c1):
             nums[k] = 1
         for l in range (c0+c1,c0+c1+c2):
-            nums[l] = 2'''
-        
-        nums.sort()
+            nums[l] = 2
